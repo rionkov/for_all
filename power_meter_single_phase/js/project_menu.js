0af41137d8f7bbf42cd651ed6d31d46996abda47
rionkov <71258199+rionkov@users.noreply.github.com>
2024-09-25 07:42:01 +0300

    Add files via upload

--- power_meter_single_phase/js/project_menu.js
+++ power_meter_single_phase/js/project_menu.js
@@ -52,3 +52,17 @@ $(div)
 .prop('id', 'Dashboard')
 .attr('style', 'display:none')
 scriptTag.parentNode.appendChild(div);
+
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
