e35b58ba9619827a43523f6dfd518a7f3a43108c
rionkov <71258199+rionkov@users.noreply.github.com>
2024-09-25 10:24:48 +0300

    Add files via upload

---
+++ power_meter_single_phase/js/project_languages.js
@@ -0,0 +1,9 @@
+scriptTag = document.scripts[document.scripts.length - 1];
+bul='Български'
+eng='English'
+select = document.createElement("select");
+$(select)
+.prop('id', 'languageSwitcher')
+.append($('<option>', { value: 'bg', text: bul }))
+.append($('<option>', { value: 'en', text: eng }));
+scriptTag.parentNode.appendChild(select);
