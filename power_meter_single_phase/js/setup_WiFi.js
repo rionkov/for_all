a65619bf5ac054d3fadba9f22c5bdffc10516590
rionkov <71258199+rionkov@users.noreply.github.com>
2024-09-30 17:48:32 +0300

    Add files via upload

--- power_meter_single_phase/js/setup_WiFi.js
+++ power_meter_single_phase/js/setup_WiFi.js
@@ -11,7 +11,11 @@ wifi_ssid_text=wifi_ssid+'_text'
 wifi_ssid_title=wifi_ssid+'_title'
 wifi_ssid_placeholder=wifi_ssid+'_placeholder'
 wifi_pass=translate_prefix+'wifi_pass'
+
 wifi_pass_id=id_prefix+'2'
+wifi_pass_id_icon=id_prefix+'2_icon'
+
+
 wifi_pass_text=wifi_pass+'_text'
 wifi_pass_title=wifi_pass+'_title'
 wifi_pass_placeholder=wifi_pass+'_placeholder'
@@ -57,40 +61,74 @@ $(div)
 .addClass('w3-label w3-text-teal bold italic underline')
 .attr('data-i18n', wifi_ssid_text)
 .attr('data-i18n-title', wifi_ssid_title)
-.prop('for', wifi_ssid_id)
-.prop('title', wifi_ssid_title)
+.attr('for', wifi_ssid_id)
+.attr('title', wifi_ssid_title)
 )
 .append($('<input>')
 .addClass('w3-input w3-animate-input w3-border w3-round')
 .attr('data-i18n-title', wifi_ssid_title)
 .attr('data-i18n-placeholder', wifi_ssid_placeholder)
-.prop('style', 'width:30%')
-.prop('type', 'text')
-.prop('placeholder', 'your SSID')
-.prop('maxlength','16')
-.prop('title', wifi_ssid_title)
-.prop('id', wifi_ssid_id))
+.attr('style', 'width:30%')
+.attr('type', 'text')
+.attr('placeholder', 'your SSID')
+.attr('maxlength','16')
+.attr('title', wifi_ssid_title)
+.attr('id', wifi_ssid_id))
 )
+
+
+
 .append($('<div>')
-.addClass('w3-half')
+.addClass('w3-half password-box')
+
+.append($('<div>')
+.addClass('password-box')
+
 .append($('<label>')
+//.addClass('password-icon')
 .attr('data-i18n', wifi_pass_text)
 .attr('data-i18n-title', wifi_pass_title)
 .text('Password :')
-.prop('for', wifi_pass_id)
-.prop('title', wifi_pass_title))
+.attr('onclick', 'show_password()')
+.attr('for', wifi_pass_id)
+.attr('title', wifi_pass_title)
+)
+
+
+.append($('<i>')
+//.addClass('fa fa-eye password-icon')
+.addClass('fa fa-eye')
+.attr('onclick', 'show_password()')
+.attr('id', wifi_pass_id_icon)
+)
+
+)
+
+
+
 .append($('<input>')
-.prop('type', 'text')
+.attr('type', 'text')
 .addClass('w3-input w3-animate-input w3-border w3-round')
 .attr('data-i18n-title', wifi_pass_title)
 .attr('data-i18n-placeholder', wifi_pass_placeholder)
-.prop('autocomplete', 'password')
-.prop('style', 'width:30%')
-.prop('placeholder', 'your password')
-.prop('maxlength','16')
-.prop('title', wifi_pass_title)
-.prop('id', wifi_pass_id))
+.attr('autocomplete', 'password')
+.attr('style', 'width:30%')
+.attr('placeholder', 'your password')
+.attr('maxlength','16')
+.attr('title', wifi_pass_title)
+.attr('id', wifi_pass_id))
+
+
+
+//)
+
+
 )
+
+
+
+
+
 )
 .append($('<div>')
  .addClass('w3-row-padding')
@@ -98,13 +136,13 @@ $(div)
  .addClass('w3-half')
 .append($('<input>')
  .addClass('w3-check')
- .prop('type', 'checkbox')
+ .attr('type', 'checkbox')
  .attr('data-i18n-title', wifi_dhcp_title)
- .prop('id', wifi_dhcp_id))
+ .attr('id', wifi_dhcp_id))
 .append($('<label>')
  .attr('data-i18n', wifi_dhcp_text)
  .attr('data-i18n-title', wifi_dhcp_title)
- .prop('for', wifi_dhcp_id)
+ .attr('for', wifi_dhcp_id)
  .text(' DHCP'))
 )
 .append($('<div>')// </div> static IP
@@ -113,17 +151,17 @@ $(div)
 .attr('data-i18n', wifi_ip_text)
 .attr('data-i18n-title', wifi_ip_title)
 .text('IP :')
-.prop('for', wifi_ip_id)
-.prop('title', wifi_ip_title))
+.attr('for', wifi_ip_id)
+.attr('title', wifi_ip_title))
 .append($('<input>')
 .addClass('w3-input w3-animate-input w3-border w3-round')
 .attr('data-i18n-title', wifi_ip_title)
 .attr('data-i18n-placeholder', wifi_ip_placeholder)
-.prop('style', stattic_elements_width)
-.prop('type', 'text')
-.prop('placeholder', 'XXX.XXX.XXX.XXX')
-.prop('title', wifi_ip_title)
-.prop('id', wifi_ip_id))
+.attr('style', stattic_elements_width)
+.attr('type', 'text')
+.attr('placeholder', 'XXX.XXX.XXX.XXX')
+.attr('title', wifi_ip_title)
+.attr('id', wifi_ip_id))
 )
 )
 .append($('<div>')
@@ -134,17 +172,17 @@ $(div)
 .attr('data-i18n', wifi_masc_text)
 .attr('data-i18n-title', wifi_masc_title)
 .text('Masc :')
-.prop('for', wifi_masc_id)
-.prop('title', wifi_masc_title))
+.attr('for', wifi_masc_id)
+.attr('title', wifi_masc_title))
 .append($('<input>')
 .addClass('w3-input w3-animate-input w3-border w3-round')
 .attr('data-i18n-title', wifi_masc_title)
 .attr('data-i18n-placeholder', wifi_masc_placeholder)
-.prop('style', stattic_elements_width)
-.prop('type', 'text')
-.prop('placeholder', '255.255.255.0')
-.prop('title', wifi_masc_title)
-.prop('id', wifi_masc_id))
+.attr('style', stattic_elements_width)
+.attr('type', 'text')
+.attr('placeholder', '255.255.255.0')
+.attr('title', wifi_masc_title)
+.attr('id', wifi_masc_id))
 )
 .append($('<div>')
 .addClass('w3-half hide')
@@ -152,17 +190,17 @@ $(div)
 .attr('data-i18n', wifi_gateway_text)
 .attr('data-i18n-title', wifi_gateway_title)
 .text('Gateway :')
-.prop('for', wifi_gateway_id)
-.prop('title', wifi_gateway_title))
+.attr('for', wifi_gateway_id)
+.attr('title', wifi_gateway_title))
 .append($('<input>')
 .addClass('w3-input w3-animate-input w3-border w3-round')
 .attr('data-i18n-title', wifi_gateway_title)
 .attr('data-i18n-placeholder', wifi_gateway_placeholder)
-.prop('style', stattic_elements_width)
-.prop('type', 'text')
-.prop('placeholder', 'XXX.XXX.XXX.XXX')
-.prop('title', wifi_gateway_title)
-.prop('id', wifi_gateway_id))
+.attr('style', stattic_elements_width)
+.attr('type', 'text')
+.attr('placeholder', 'XXX.XXX.XXX.XXX')
+.attr('title', wifi_gateway_title)
+.attr('id', wifi_gateway_id))
 )
 )
 .append($('<div>')
@@ -173,17 +211,17 @@ $(div)
 .attr('data-i18n', wifi_dns1_text)
 .attr('data-i18n-title', wifi_dns1_title)
 .text('Masc :')
-.prop('for', wifi_dns1_id)
-.prop('title', wifi_dns1_title))
+.attr('for', wifi_dns1_id)
+.attr('title', wifi_dns1_title))
 .append($('<input>')
 .addClass('w3-input w3-animate-input w3-border w3-round')
 .attr('data-i18n-title', wifi_dns1_title)
 .attr('data-i18n-placeholder', wifi_dns1_placeholder)
-.prop('style', stattic_elements_width)
-.prop('type', 'text')
-.prop('placeholder', 'XXX.XXX.XXX.XXX')
-.prop('title', wifi_dns1_title)
-.prop('id', wifi_dns1_id))
+.attr('style', stattic_elements_width)
+.attr('type', 'text')
+.attr('placeholder', 'XXX.XXX.XXX.XXX')
+.attr('title', wifi_dns1_title)
+.attr('id', wifi_dns1_id))
 )
 .append($('<div>')
 .addClass('w3-half hide')
@@ -191,17 +229,17 @@ $(div)
 .attr('data-i18n', wifi_dns2_text)
 .attr('data-i18n-title', wifi_dns2_title)
 .text('Gateway :')
-.prop('for', wifi_dns2_id)
-.prop('title', wifi_dns2_title))
+.attr('for', wifi_dns2_id)
+.attr('title', wifi_dns2_title))
 .append($('<input>')
 .addClass('w3-input w3-animate-input w3-border w3-round')
 .attr('data-i18n-title', wifi_dns2_title)
 .attr('data-i18n-placeholder', wifi_dns2_placeholder)
-.prop('style', stattic_elements_width)
-.prop('type', 'text')
-.prop('placeholder', 'XXX.XXX.XXX.XXX')
-.prop('title', wifi_dns2_title)
-.prop('id', wifi_dns2_id))
+.attr('style', stattic_elements_width)
+.attr('type', 'text')
+.attr('placeholder', 'XXX.XXX.XXX.XXX')
+.attr('title', wifi_dns2_title)
+.attr('id', wifi_dns2_id))
 )
 )
 document.getElementById(appendTo).appendChild(div);
@@ -223,3 +261,21 @@ $('#' + wifi_dhcp_id).on('change', function() {
 		$('.hide').slideDown();
     }
 });
+
+function show_password() {
+  var x = document.getElementById(wifi_pass_id);
+  var y = document.getElementById(wifi_pass_id_icon);
+  if (x.type === "password") {
+$(y).toggleClass("fa-eye fa-eye-slash");
+    x.type = "text";
+  } else {
+$(y).toggleClass("fa-eye-slash fa-eye");
+    x.type = "password";
+  }
+  x.focus();
+};
+
+function parse_setup_wifi(msg){
+
+}
+
