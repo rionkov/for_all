b307fdef90a3dfe69600023f37d3ec59d003bc8e
rionkov <71258199+rionkov@users.noreply.github.com>
2024-09-30 18:17:33 +0300

    Add files via upload

---
+++ power_meter_single_phase/js/project_index.js
@@ -0,0 +1,36 @@
+scriptTag = document.scripts[document.scripts.length - 1];
+appendTo = '<head>'
+
+
+$.getScript('https://cdn.jsdelivr.net/gh/rionkov/for_all/power_meter_single_phase/js/project_title.min.js')
+
+$('<link/>', {
+   rel: 'stylesheet',
+   //type: 'text/css',
+   href: 'https://cdnjs.cloudflare.com/ajax/libs/w3-css/4.1.0/w3.min.css'
+}).appendTo('head')
+$('<link/>', {
+   rel: 'stylesheet',
+   //type: 'text/css',
+   href: 'https://www.w3schools.com/lib/w3-theme-teal.css'
+}).appendTo('head')
+$('<link/>', {
+   rel: 'stylesheet',
+   //type: 'text/css',
+   href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+}).appendTo('head')
+
+
+$('<link/>', {
+   rel: 'icon',
+   //type: 'text/css',
+   href: 'https://cdn.jsdelivr.net/gh/rionkov/for_all/power_meter_single_phase/favicon.ico'
+}).appendTo('head')
+
+$('<link/>', {
+   rel: 'stylesheet',
+   //type: 'text/css',
+   href: 'https://cdn.jsdelivr.net/gh/rionkov/for_all/power_meter_single_phase/css/project_css.min.css'
+}).appendTo('head')
+
+
